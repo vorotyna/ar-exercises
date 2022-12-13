f952ad85c6bf39aaa5e04846fa53081f1839a201
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-13 02:52:43 +0000

    Delete store from db

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,6 +1,5 @@
 require_relative '../setup'
 require_relative './exercise_1'
-require 'pp'
 
 puts "Exercise 2"
 puts "----------"
@@ -8,9 +7,3 @@ puts "----------"
 @store1 = Store.find(1)
 @store1.update(name: "Gucci")
 @store2 = Store.find(1)
-
-
-
-# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
-# 2. Load the second store from the database and assign it to `@store2`.
-# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,16 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
+require 'pp'
 
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+pp @store3 = Store.find(3)
+@store3.destroy
+puts Store.count
+
+
+# 1. Load the third store (into `@store3`) as you did the other two before.
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
