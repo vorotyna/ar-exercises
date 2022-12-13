3577e25f8a8b8aa7f67d0cafe3a9324754ab8b58
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-13 03:07:43 +0000

    Create new stores, fetch collection of store where..., looping through data

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,16 +1,10 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
-require 'pp'
 
 puts "Exercise 3"
 puts "----------"
 
-pp @store3 = Store.find(3)
+@store3 = Store.find(3)
 @store3.destroy
 puts Store.count
-
-
-# 1. Load the third store (into `@store3`) as you did the other two before.
-# 2. Using Active Record's `destroy` method, delete the store from the database.
-# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,14 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+stores = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+stores = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+stores = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for mens_store in @mens_stores do
+  puts "Name: #{mens_store.name}, Annual Revenue: #{mens_store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: ...1000000)
