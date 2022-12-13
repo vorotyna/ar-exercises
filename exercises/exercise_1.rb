fbbbc2eb478f9112c0ac5cb60e8c044bba479c84
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-13 02:36:22 +0000

    Create stores in db, count number of stores in db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,8 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+stores = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+stores = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+stores = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
