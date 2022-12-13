33a1ac4e82a613f3a97001193c404b0f652d8b01
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-12-13 03:40:29 +0000

    Join two tables, add employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Liso", last_name: "Piso", hourly_rate: 100)
+@store1.employees.create(first_name: "Luko", last_name: "Puko", hourly_rate: 110)
+@store2.employees.create(first_name: "Maso", last_name: "Paso", hourly_rate: 20)
+@store2.employees.create(first_name: "Daro", last_name: "Paro", hourly_rate: 40)
+@store2.employees.create(first_name: "Nafo", last_name: "Pafo", hourly_rate: 11)
